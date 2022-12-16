5f5575eae3567855b2766d0c184d111dc14fa3f5
emsiru <119374566+emsiru@users.noreply.github.com>
2022-12-16 09:07:03 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,35 @@
+from dash import html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+import dash_auth
+
+# Connect to main app.py file
+from app import app
+from app import server
+
+from apps import navigationadv, graph, dashboard_layout
+
+# url = "opc.tcp://192.168.0.30:4840"
+# client = Client(url)
+
+
+# auth = dash_auth.BasicAuth(
+#     app, {'admin':'1'}
+# )
+
+app.layout = html.Div([
+    html.Div(navigationadv.navbar),
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content', children=[])
+])
+
+@app.callback(Output('page-content', 'children'),   
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/graph':
+        return graph.layout
+    else:
+        return dashboard_layout.layout
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port = 3000)
